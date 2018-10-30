6f45fcbfa21ccf57db4cb4abab43ebbee53b20f5
Andrea/Rodriguez/Germann <andrearodriguezgermann@gmaiil.com>
2018-10-30 10:55:47 +0100

    Homework week 3 - 2nd part - changes in database.js and index.js file: async

--- database.js
+++ database.js
@@ -1,12 +1,26 @@
 const fs = require('fs')
 const CircularJSON = require('circular-json')
 
+async function save(data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./data.json', CircularJSON.stringify(data), (err, file) => {
+            if (err) return reject(err)
+            resolve()
+        })
+    })
+}
+    
+async function load(filename) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, file) => {
+            if (err) return reject(err)
+            const data = CircularJSON.parse(file)
+            resolve(data)
+       })
+    })
+}
+    
 module.exports = {
-   save(data){
-   fs.writeFileSync('data.json', CircularJSON.stringify(data))
-   },
-
-   load() {
-       return CircularJSON.parse(fs.readFileSync('data.json'))
-   }
+    save,
+    load
 }
--- index.js
+++ index.js
@@ -3,30 +3,42 @@ const Tweet = require('./tweet')
 const Like = require('./like')
 const Database = require('./database')
 
-const andrea = new User('Andrea')
-const anna = new User('Anna')
-const tom = new User('Tom')
-const tweetOne = new Tweet(andrea, 'Hello world')
-const tweetTwo = new Tweet(andrea, 'Second one')
-const tweetThree = new Tweet(tom, "First Tom's tweet")
-const likeOne = new Like(anna, tweetOne)
-const likeTwo = new Like(anna, tweetTwo)
-const likeThree = new Like(tom, tweetOne)
-const likeFour = new Like(andrea, tweetThree)
-const likeFive = new Like(anna, tweetThree)
+const main = async () => {
+    const andrea = new User('Andrea')
+    const anna = new User('Anna')
+    const tom = new User('Tom')
+    const tweetOne = new Tweet(andrea, 'Hello world')
+    const tweetTwo = new Tweet(andrea, 'Second one')
+    const tweetThree = new Tweet(tom, "First Tom's tweet")
+    const likeOne = new Like(anna, tweetOne)
+    const likeTwo = new Like(anna, tweetTwo)
+    const likeThree = new Like(tom, tweetOne)
+    const likeFour = new Like(andrea, tweetThree)
+    const likeFive = new Like(anna, tweetThree)
 
-andrea.create(tweetOne)
-likeOne.like(anna, tweetOne)
-andrea.create(tweetTwo)
-likeTwo.like(anna, tweetTwo)
-likeThree.like(tom, tweetOne)
-likeTwo.dislike(anna, tweetTwo)
-tom.create(tweetThree)
-likeFour.like(andrea, tweetThree)
-likeFive.like(anna, tweetThree)
+    andrea.create(tweetOne)
+    likeOne.like(anna, tweetOne)
+    andrea.create(tweetTwo)
+    likeTwo.like(anna, tweetTwo)
+    likeThree.like(tom, tweetOne)
+    likeTwo.dislike(anna, tweetTwo)
+    tom.create(tweetThree)
+    likeFour.like(andrea, tweetThree)
+    likeFive.like(anna, tweetThree)
 
-Database.save([tweetOne, tweetTwo, tweetThree])
-const loadedTweets = Database.load()
-const tweetInstances = loadedTweets.map(Tweet.create)
-console.log(tweetInstances)
+    await Database.save([tweetOne, tweetTwo, tweetThree])
+    const loadedTweets = await Database.load('./data.json')
+    const tweetInstances = loadedTweets.map(Tweet.create)
+    console.log(tweetInstances)
+}
+
+(async () => {
+    try {
+        await main()
+    } catch (e) {
+        console.log(e)
+    }
+  })()
+
+
 
